8b85b8b07fa052a99c0326867ae90202067c39cd
Arhik <arhik23@gmail.com>
2024-01-25 23:47:55 +0530

    snapshot

--- src/compiler/execution.jl
+++ src/compiler/execution.jl
@@ -7,13 +7,44 @@ using Lazy
 # TODO remove
 using WGPUCompute
 using Infiltrator
+using MLStyle
+using MLStyle: @match
+using JuliaVariables
+
+function WGSLTypes.wgslType(a::JuliaVariables.Var)
+	if a.is_mutable && !(a.is_global)
+		if isdefined(Base, a.name)
+			return a.name
+		else
+			return :(@var $(a.name))
+		end
+	elseif a.is_global
+		return a.name
+	elseif !(a.is_global)
+		return a.name
+	else
+		@error "This JuliaVariable is not captured"
+	end
+end
+
+# copied from https://github.com/JuliaStaging/JuliaVariables.jl
+unwrap_scoped(ex) =
+	@match ex begin
+	    Expr(:scoped, _, a) => unwrap_scoped(a)
+	    Expr(head, args...) => Expr(head, map(unwrap_scoped, args)...)
+	    a => a
+	end
+
+resolve! = unwrap_scoped ∘ MacroTools.striplines ∘ solve! ∘ simplify_ex
 
 function getShaderCode(f, args::WgpuArray{T, N}...) where {T, N}
 	fexpr = @code_string(f(args...)) |> Meta.parse
 	@capture(fexpr, @wgpukernel workgroupSizes_ workgroupCount_ function fname_(fargs__) where Targs__ fbody__ end)
+
 	workgroupSizes = Meta.eval(workgroupSizes)
 	workgroupCount = Meta.eval(workgroupCount)
 	originArgs = fargs[:]
+	
 	builtinArgs = [
 		:(@builtin(global_invocation_id, global_id::Vec3{UInt32})),
 		:(@builtin(local_invocation_id, local_id::Vec3{UInt32})),
@@ -21,30 +52,6 @@ function getShaderCode(f, args::WgpuArray{T, N}...) where {T, N}
 		:(@builtin(workgroup_id, workgroup_id::Vec3{UInt32})),
 	]
 	
-	"""
-	
-	# This section needs to be generated too
-	# Since kernels can have more than one input
-	# we need to accomodate for that.
-	
-    code = quote
-		struct IOArray
-			data::WArray{$T}
-		end
-		
-		struct IOArrayVector
-			data::WArray{Vec4{$T}}
-		end
-		
-		struct IOArrayMatrix
-			data::WArray{Mat4{$T}}
-		end
-		
-		@var StorageReadWrite 0 0 input0::IOArray
-		@var StorageReadWrite 0 1 output0::IOArray
-    end
-    
-    """
     # TODO used `repeat` since `ones` is causing issues.
     # interesting bug to raise.
     if workgroupSizes |> length < 3
@@ -57,183 +64,31 @@ function getShaderCode(f, args::WgpuArray{T, N}...) where {T, N}
 			data::WArray{$T}
 		end
 	end
-	
-	cntxt = emitWGSLJuliaBody(fbody, fargs)
 
-	fquote = quote
-		function $(fname)($(builtinArgs...))
-			$((cntxt.stmnts)...)
-		end
-	end
-	
-	push!(code.args, cntxt.globals...)
-	
-    push!(code.args,
-		:(@compute @workgroupSize($(workgroupSizes...)) $(fquote.args...))
-   	)
-	
-    return code
-end
-
-"""
-macro tt(func)
-	@show func
-	fexpr = @code_expr(call(func))
-end
-
-macro wgpukernel(workgroupsizeExpr, dispatchExpr, func)
-	@capture(func, f_(x_))
-	wgpu(workgroupsizeExpr, dispatchExpr, func)
-end
-"""
-
-mutable struct KernelContext
-	inargs::Dict{Symbol, Any}
-	outargs::Dict{Symbol, Any}
-	tmpargs::Array{Symbol}
-	typeargs::Array{Symbol}
-	stmnts::Array{Expr}
-	globals::Array{Expr}
-	indent::Int # Helps debugging
-	kernel # TODO function body with all statements will reside here
-end
-
-# @forward KernelContext.args push!
-function emitWGSLJuliaBody(fbody, inargs)
-	ins = Dict{Symbol, Any}()
-	outs = Dict{Symbol, Any}()
-	
-	cntxt = KernelContext(ins, outs, Symbol[], Symbol[], Expr[], Expr[], 0, nothing)
-	
-	for (idx, arg) in enumerate(inargs)
+	for (idx, arg) in enumerate(fargs)
 		if @capture(arg, a_::b_)
-			iovar = Symbol(:input, idx-1)
 			push!(
-				cntxt.globals,
+				code.args,
 				quote
-					@var StorageReadWrite 0 $(idx-1) $(iovar)::IOArray
-				end
+					@var StorageReadWrite 0 $(idx-1) $a::IOArray
+				end |> unblock
 			)
-			ins[a] = iovar
 		else
 			@error "Could not capture input arguments"
 		end
 	end
+
+	fquote = quote
+		function $(fname)($(builtinArgs...))
+			$(fbody...)
+		end
+	end |> resolve!
+
+    push!(code.args,
+		:(@compute @workgroupSize($(workgroupSizes...)) $(fquote.args...))
+   	)
 	
-	# TODO this is stupid but good first implementation maybe
-	# This assumes that the output argument is lhs of last stmnt
-	#if @capture(fbody[end], a_[b_] = c_) || @capture(fbody[end], a_=b_)
-	#	idx = length(ins)
-	#	iovar = Symbol(:output, idx)
-	#	outs[a] = iovar
-	#	push!(
-	#		cntxt.globals, 
-	#		quote
-	#			@var StorageReadWrite 0 $(idx) $(iovar)::IOArray
-	#		end
-	#	)
-	#elseif false
-		# TODO captures others like return statements
-		# TODO or just symbol
-	#end
-	
-	wgslFunctionStatements(cntxt, fbody)
-	cntxt
-end
-
-function wgslAssignment(expr::Expr, prefix::Union{Nothing, Symbol})
-	@capture(expr, a_ = b_) || error("Expecting simple assignment a = b")
-	return ifelse(prefix==:let, :(@let $a = $b), :($a = $b))
-end
-
-
-function wgslFunctionStatements(cntxt, stmnts)
-	for (i, stmnt) in enumerate(stmnts)
-		wgslFunctionStatement(cntxt, stmnt; isLast=(length(stmnts) == i))
-	end
-end
-
-function wgslFunctionStatement(cntxt::KernelContext, stmnt; isLast = false)
-	if @capture(stmnt, a_[b_] = c_)
-		stmnt = :($(wgslFunctionStatement(cntxt, a))[$(wgslFunctionStatement(cntxt, b))] = $(wgslFunctionStatement(cntxt, c)))
-		push!(cntxt.stmnts, wgslAssignment(stmnt, nothing))
-	elseif @capture(stmnt, a_ = b_)
-		if (a in cntxt.tmpargs) && !(a in cntxt.inargs |> keys) && !(a in cntxt.outargs |> keys)
-			stmnt = :($(wgslFunctionStatement(cntxt, a)) = $(wgslFunctionStatement(cntxt, b)))
-			push!(cntxt.stmnts, wgslAssignment(stmnt, nothing))
-		else
-			push!(cntxt.tmpargs, a)
-			stmnt = :($(wgslFunctionStatement(cntxt, a)) = $(wgslFunctionStatement(cntxt, b)))
-			push!(cntxt.stmnts, wgslAssignment(stmnt, :let))
-		end
-	elseif @capture(stmnt, a_.b_)
-		return :($(wgslFunctionStatement(cntxt, a)).$b)
-	elseif typeof(stmnt) == Symbol
-		if stmnt == :globalId # TODO 
-			return :global_id
-		elseif stmnt == :dispatchDims # TODO 
-			return :num_workgroups
-		elseif stmnt == :localId
-			return :local_id
-		elseif stmnt == :workgroupId
-			return :workgroup_id
-		elseif stmnt == :workgroupDims
-			return :num_workgroups
-		end
-		if stmnt in cntxt.tmpargs && !(stmnt in cntxt.inargs |> keys) && !(stmnt in cntxt.outargs |> keys)
-			return stmnt
-		elseif (stmnt in cntxt.inargs |> keys)
-			return :($(cntxt.inargs[stmnt]).data)
-		elseif (stmnt in cntxt.outargs |> keys)
-			iovar = Symbol(:output, length(cntxt.outargs |> keys) + 1)
-			return :($(cntxt.outargs[stmnt]).data)
-		else
-			@error "Something is not right with $stmnt expr"
-		end
-	elseif typeof(stmnt) <: Number
-		return stmnt
-	elseif @capture(stmnt, a_[b_])
-		asub = wgslFunctionStatement(cntxt, a)
-		bsub = wgslFunctionStatement(cntxt, b)
-		return :($asub[$bsub])
-	elseif @capture(stmnt, @let t_ | @let t__)
-		push!(cntxt.stmnts, stmnt)
-	elseif @capture(stmnt, @var t_ | @let t__)
-		push!(cntxt.stmnts, stmnt)
-	elseif @capture(stmnt, a_ += b_)
-		wgslFunctionStatement(cntxt, :($a = $(wgslFunctionStatement(cntxt, a)) + $(wgslFunctionStatement(cntxt, b))))
-	elseif @capture(stmnt, a_ -= b_)
-		wgslFunctionStatement(cntxt, :($a = $(wgslFunctionStatement(cntxt, a)) - $(wgslFunctionStatement(cntxt, b))))
-	elseif @capture(stmnt, a_ *= b_)
-		wgslFunctionStatement(cntxt, :($a = $(wgslFunctionStatement(cntxt, a)) * $(wgslFunctionStatement(cntxt, b))))
-	elseif @capture(stmnt, a_ /= b_)
-		wgslFunctionStatement(cntxt, :($a = $(wgslFunctionStatement(cntxt, a)) / $(wgslFunctionStatement(cntxt, b))))
-	elseif @capture(stmnt, f_(x_, y_))
-		if f in (:*, :-, :+, :/)
-			x = wgslFunctionStatement(cntxt, x)
-			y = wgslFunctionStatement(cntxt, y)
-			return :($f($x, $y))
-		else
-			return :($f($x, $y))
-		end
-	elseif @capture(stmnt, f_(x__))
-		x = tuple(x...)
-		return :($f($(x...)))
-	elseif @capture(stmnt, return t_)
-		push!(cntxt.stmnts, (wgslType(t)))
-	elseif @capture(stmnt, if cond_ ifblock__ end)
-		if cond == true
-			wgslFunctionStatements(io, ifblock)
-		end
-	elseif @capture(stmnt, if cond_ ifBlock__ else elseBlock__ end)
-		if eval(cond) == true
-			wgslFunctionStatements(io, ifBlock)
-		else
-			wgslFunctionStatements(io, elseBlock)
-		end
-	else
-		@error "Failed to capture statment : $stmnt !!"
-	end
+    return (code |> MacroTools.striplines)
 end
 
 function compileShader(f, args::WgpuArray{T, N}...) where {T, N}
