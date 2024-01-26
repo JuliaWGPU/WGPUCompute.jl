a1e1260e653c3a8854406e934b450a61aaf39591
Arhik <arhik23@gmail.com>
2024-01-26 17:37:34 +0530

    cast_kernel example

---
+++ examples/cast_kernel.jl
@@ -0,0 +1,19 @@
+using WGPUCompute
+
+@wgpukernel workgroupSizes=(4, 4) workgroupCount=(2, 2) function cast_kernel(x::WgpuArray{T, N}, out::WgpuArray{S, N}) where {T, S, N}
+	xdim = workgroupDims.x
+	ydim = workgroupDims.y
+	gIdx = workgroupId.x*xdim + localId.x
+	gIdy = workgroupId.y*ydim + localId.y
+	gId = xDims.x*gIdy + gIdx
+	out[gId] = UInt32(ceil(x[gId]))
+end
+
+function cast(S::DataType, x::WgpuArray{T, N}) where {T, N}
+	y = WgpuArray{S}(undef, size(x))
+	cast_kernel(x, y)
+	return y
+end
+
+x = WgpuArray{Float32}(rand(Float32, 8, 8) .- 0.5f0)
+z = cast(UInt32, x)
