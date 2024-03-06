8986208a509cbfe29d0f4552c6263724779c67ef
Arhik <arhik23@gmail.com>
2024-03-06 13:14:17 +0530

    Create cast.jl
    First elementwise kernel which was an example but decided to ship it as an `op` kernel.

---
+++ src/ops/cast.jl
@@ -0,0 +1,15 @@
+function cast_kernel(x::WgpuArray{T, N}, out::WgpuArray{S, N}) where {T, S, N}
+	xdim = workgroupDims.x
+	ydim = workgroupDims.y
+	gIdx = workgroupId.x*xdim + localId.x
+	gIdy = workgroupId.y*ydim + localId.y
+	gId = xDims.x*gIdy + gIdx
+	out[gId] = S(ceil(x[gId]))
+end	
+
+function cast(S::DataType, x::WgpuArray{T, N}) where {T, N}
+	y = WgpuArray{S}(undef, size(x))
+	@wgpukernel launch=true workgroupSizes=(4, 4) workgroupCount=(2, 2) cast_kernel(x, y)
+	return y
+end	
+
