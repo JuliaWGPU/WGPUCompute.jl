221374811d53b70bbe7fcd102e9c1b6e41cb99c4
Arhik <arhik23@gmail.com>
2024-03-24 21:29:00 +0530

    broadcast kernel accepts function
    fixes https://github.com/JuliaWGPU/WGPUCompute.jl/issues/14

--- src/ops/elemwise.jl
+++ src/ops/elemwise.jl
@@ -1,24 +1,26 @@
-export broadcast_kernel, elemwise
 
-function broadcast_kernel_gen(f::Function, a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N}
-	:(function broadcast_kernel(a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N}
-		xdim = workgroupDims.x
-		ydim = workgroupDims.y
-		gIdx = workgroupId.x*xdim + localId.x
-		gIdy = workgroupId.y*ydim + localId.y
-		gId = xDims.x*gIdy + gIdx
-		out[gIdx] = $f(x[gIdx], y[gIdx])
-	end)
+function broadcast_kernel(op::Function, x::WgpuArray{T, N}, y::WgpuArray{T, N}, out::WgpuArray{T, N}) where {T, N}
+	xdim = workgroupDims.x
+	ydim = workgroupDims.y
+	gIdx = workgroupId.x*xdim + localId.x
+	gIdy = workgroupId.y*ydim + localId.y
+	gId = xDims.x*gIdy + gIdx
+	out[gId] = op(x[gId], y[gId])
 end
 
-function elemwise(f::Function, a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N}
-	broadcast_kernel = eval(@__MODULE__, broadcast_kernel_gen(f, a, b))
+function elwise(f::Function, a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N}
 	out = similar(a)
-	@wgpukernel launch=true workgroupSizes=size(out) workgroupCount=(1, 1) broadcast_kernel(a, b, out)
-	return out	
+	@wgpukernel launch=true workgroupSizes=size(out) workgroupCount=(1, 1) broadcast_kernel(f, a, b, out)
+	return out
 end
 
-Base.:+(a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N} = begin
-	elemwise(+, a, b)
-end
+Base.:+(a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N} = elwise(+, a, b)
+Base.:-(a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N} = elwise(-, a, b)
+Base.:*(a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N} = elwise(*, a, b)
+Base.:/(a::WgpuArray{T, N}, b::WgpuArray{T, N}) where {T, N} = elwise(/, a, b)
+
+
+using Revise
+using WGPUCompute
+x = WgpuArray{Float32, 2}(rand(16, 16))
 
