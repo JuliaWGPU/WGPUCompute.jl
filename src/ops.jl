bc717db5dbbf521ee30510a7a76fadf8e6b9975c
Arhik <arhik23@gmail.com>
2024-04-06 15:53:41 +0530

    WIP elemwise

--- src/ops.jl
+++ src/ops.jl
@@ -1,5 +1,8 @@
 include("ops/matmul.jl")
 include("ops/transpose.jl")
+include("ops/cast.jl")
+include("ops/clamp.jl")
+include("ops/elemwise.jl")
 
 """
 for (root, dirs, files) in walkdir(joinpath(@__DIR__, "ops"))
