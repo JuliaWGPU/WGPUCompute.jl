c1087eb9612236f0f464056b01e697aee2b08a5a
Arhik <arhik23@gmail.com>
2024-04-12 19:20:01 +0530

    [DOC] update ops/cast.jl
    Documentation

--- src/ops/cast.jl
+++ src/ops/cast.jl
@@ -1,5 +1,12 @@
 export cast_kernel, cast
 
+"""
+	cast_kernel(x::WgpuArray{T, N}, out::WgpuArray{S, N}) where {T, S, N}
+This is a compute kernel which casts the `x` array of eltype `T` to eltype `S`.
+Users are not supposed to use this function call from julia. This instead needs to 
+be wrapped with an additional function which uses `@wgpukernel` macro call to
+convert the julia function definition to a equivalent `WGPU` kernel function.
+"""
 function cast_kernel(x::WgpuArray{T, N}, out::WgpuArray{S, N}) where {T, S, N}
 	xdim = workgroupDims.x
 	ydim = workgroupDims.y
@@ -9,9 +16,13 @@ function cast_kernel(x::WgpuArray{T, N}, out::WgpuArray{S, N}) where {T, S, N}
 	out[gId] = S(ceil(x[gId]))
 end	
 
+"""
+	cast(S::DataType, x::WgpuArray{T, N}) where {T, N}
+This is a wrapper function for `cast_kernel` kernel function. This is meant 
+for users to `cast` from regular julia functions.
+"""
 function cast(S::DataType, x::WgpuArray{T, N}) where {T, N}
 	y = WgpuArray{S}(undef, size(x))
 	@wgpukernel launch=true workgroupSizes=(4, 4) workgroupCount=(2, 2) shmem=() cast_kernel(x, y)
 	return y
 end	
-
